b113b07b4398d6baa6feb919671a6a5cdb042cdf
paras <parasrai@Jarvis.local>
2022-07-23 22:41:03 +0545

    Final

--- src/App.js
+++ src/App.js
@@ -22,26 +22,29 @@ function App() {
 
   return (
     <div className="App d-flex justify-content-center">
-      <div className={starStyles.stars}></div>
-      <div className={starStyles.twinkling}></div>
-      <div className={meterStyle.meteor_1}></div>
-      <div className={meterStyle.meteor_2}></div>
-      <div className={meterStyle.meteor_3}></div>
-      <div className={meterStyle.meteor_4}></div>
-      <div className={meterStyle.meteor_5}></div>
-      <div className={meterStyle.meteor_6}></div>
-      <div className={meterStyle.meteor_7}></div>
-      <div className={meterStyle.meteor_8}></div>
-      <div className={meterStyle.meteor_9}></div>
-      <div className={meterStyle.meteor_10}></div>
-      <div className={meterStyle.meteor_11}></div>
-      <div className={meterStyle.meteor_12}></div>
-      <div className={meterStyle.meteor_13}></div>
-      <div className={meterStyle.meteor_14}></div>
-      <div className={meterStyle.meteor_15}></div>
-      <div className={meterStyle.meteor_16}></div>
+      <div className={starStyles.container}>
+
+        <div className={starStyles.stars}></div>
+        <div className={starStyles.twinkling}></div>
+        <div className={meterStyle.meteor_1}></div>
+        <div className={meterStyle.meteor_2}></div>
+        <div className={meterStyle.meteor_3}></div>
+        <div className={meterStyle.meteor_4}></div>
+        <div className={meterStyle.meteor_5}></div>
+        <div className={meterStyle.meteor_6}></div>
+        <div className={meterStyle.meteor_7}></div>
+        <div className={meterStyle.meteor_8}></div>
+        <div className={meterStyle.meteor_9}></div>
+        <div className={meterStyle.meteor_10}></div>
+        <div className={meterStyle.meteor_11}></div>
+        <div className={meterStyle.meteor_12}></div>
+        <div className={meterStyle.meteor_13}></div>
+        <div className={meterStyle.meteor_14}></div>
+        <div className={meterStyle.meteor_15}></div>
+        <div className={meterStyle.meteor_16}></div>
 
 
+      </div>
       <Router>
         <Routes>
           <Route path="/" element={<StartPage />} />
